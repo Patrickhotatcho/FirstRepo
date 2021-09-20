d8cfbb3948721945535ae8d67a64bd8035cd3659
Patrick <hotatchang@gmail.com>
2021-09-21 03:32:24 +0800

    update buyStock function: allow buy same stock, and add sellStock function

--- main.py
+++ main.py
@@ -9,36 +9,13 @@ import portfolio as pf
   
 
 portf1 = pf.Portfolio()
-portf1.addStock('2020-01-01','AAPL',130, 1000)
-portf1.addStock('2020-02-01','GOOG',200, 500)
-portf1.addStock('2020-03-01','NFLX',120, 500)
+portf1.buyStock('2020-01-01','AAPL',130, 1000)
+portf1.buyStock('2020-02-01','GOOG',200, 500)
+portf1.buyStock('2020-03-01','NFLX',120, 500)
+portf1.buyStock('2020-03-01','NFLX',110, 500)
+portf1.buyStock('2020-03-01','NFLX',160, 500)
+portf1.sellStock('AAPL',150,200)
+portf1.sellStock('AAPL',150,800)
 print(portf1.getPortfolio())
 
 
-
-
-'''plt.style.use("fivethirtyeight")
-
-asset = ['FB','AMZN','AAPL','NFLX','GOOG']
-weights = np.array([0.2,0.2,0.2,0.2,0.2])
-
-stockStartDate = "2019-01-01"
-today = datetime.today().strftime('%Y-%m-%d')
-print(today)
-
-df = pd.DataFrame()
-
-for stock in asset:
-    df[stock] = web.DataReader(stock, data_source='yahoo',start=stockStartDate,end= today)['Adj Close']
-
-title = 'Average Moving'
-
-portfolio = df
-
-for stock in portfolio.columns.values:
-    plt.plot(df[stock],label=stock)
-plt.title(title)
-plt.xlabel('Date', fontsize =18)
-plt.ylabel('Price(USD)',fontsize=18)
-plt.legend()
-plt.show()'''
--- portfolio.py
+++ portfolio.py
@@ -6,38 +6,66 @@ import numpy as np
 import math
 
 class Portfolio:
-    noOfStock = 0;
-    totalValue = 0;
-
-    portf = pd.DataFrame(columns=['Date','Stock','Price','No. of transaction','Value','Average Return','Volatility','Current Value'])
+    noOfStock = int(0);
+    invested = float(0);
+    turnover = float(0);
+    PandL = float(0);
+    portf = pd.DataFrame(columns=['Date','Stock','Price','No. of transaction','Value','Average Return(1yr)','Volatility(1yr)','Current Value','Spot Price',"%change"])
 
     def __init__(self):
-        self.totalValue = 0;
+        self.invested = 0;
         self.noOfStock = 0;
 
-    def addStock(self, date, stock, price, noOfTransaction):   
-        yearAgo = (datetime.today()- timedelta(365)).strftime('%Y-%m-%d')
-        today = datetime.today().strftime('%Y-%m-%d')
-        try:
-            df = web.DataReader(stock, data_source='yahoo',start= yearAgo,end= today)['Adj Close']
-        except:
-            print('Invalid Input')
-        
-        self.noOfStock += 1
-        self.totalValue += price*noOfTransaction
-        risk = str(round((self.volatility(stock,df))*100,2)) + '%'
-        AverageReturn = str(round((self.AverageReturn(stock,df)-1)*100,2)) + '%'
-        self.portf.loc[self.noOfStock-1] = [date,stock,price,noOfTransaction,(price*noOfTransaction),AverageReturn,risk,0]
-        self.updateCurrentValue(self.portf)
+    def buyStock(self, date, stock, price, noOfTransaction):
+        filt = self.portf['Stock'] == stock
+        if(self.portf.loc[filt,'Stock'].any()):
+            self.invested += price*noOfTransaction
 
-    def updateCurrentValue(self,df):
+            self.portf.loc[filt,'Price'] = (self.portf.loc[filt,'Value'] + noOfTransaction*price)/(noOfTransaction+self.portf.loc[filt,'No. of transaction'])
+            self.portf.loc[filt,'No. of transaction'] = noOfTransaction+self.portf.loc[filt,'No. of transaction']
+            self.portf.loc[filt,'Value'] = self.portf.loc[filt,'Price']*self.portf.loc[filt,'No. of transaction']
+        else:
+            self.noOfStock += 1
+            self.invested += price*noOfTransaction
+            yearAgo = (datetime.today()- timedelta(365)).strftime('%Y-%m-%d')
+            today = datetime.today().strftime('%Y-%m-%d')
+            try:
+                df = web.DataReader(stock, data_source='yahoo',start= yearAgo,end= today)['Adj Close']
+            except:
+                print('Invalid Input')
+                
+            risk = str(round((self.volatility(stock,df))*100,2)) + '%'
+            AverageReturn = str(round((self.AverageReturn(stock,df)-1)*100,2)) + '%'
+            self.portf.loc[self.noOfStock-1] = [date,stock,float(price),int(noOfTransaction),float(price*noOfTransaction),AverageReturn,risk,0,0,0]
+        self.update(self.portf)
+
+    def sellStock(self,stock,price,noOftransaction):
+        filt = self.portf['Stock'] == stock
+        if(self.portf.loc[filt,'Stock'].any()):
+            self.turnover = price*noOftransaction
+            self.PandL = (price-float(self.portf.loc[filt]['Price']))*noOftransaction
+            self.portf.loc[filt,'No. of transaction'] -= noOftransaction
+            self.portf.loc[filt,'Value'] -= price*noOftransaction
+             
+            if(int(self.portf.loc[filt,'No. of transaction']) == 0):
+                print(self.portf[self.portf['Stock']==stock].index[0])
+
+                self.portf.drop([self.portf[self.portf['Stock']==stock].index[0]])
+    
+            
+
+    def update(self,df):
         today = datetime.today().strftime('%Y-%m-%d');
         weekAgo = (datetime.today()- timedelta(7)).strftime('%Y-%m-%d')
        
         for stocks in df['Stock']:
             df = web.DataReader(stocks, data_source='yahoo',start= weekAgo,end= today)['Adj Close']
             filt = self.portf['Stock'] == stocks
-            self.portf.loc[filt,'Current Value'] = df.iloc[len(df)-1]*self.portf.loc[filt,'No. of transaction']
+            self.portf.loc[filt,'Spot Price'] = round(df.iloc[len(df)-1],2)  
+            self.portf.loc[filt,'%change'] = str(round(((df.iloc[len(df)-1] / float(self.portf.loc[filt,'Price']))-1)*100,2))+'%'
+            
+            self.portf.loc[filt,'Current Value'] = round(df.iloc[len(df)-1],2) *self.portf.loc[filt,'No. of transaction']
+        
 
     def getPortfolio(self):
         return self.portf
@@ -48,6 +76,7 @@ class Portfolio:
         b=returns.iloc[1:].values
         a = stats.gmean(b)
         return a
+
     def volatility(self,stock,df):
         df = df.pct_change()
         var = df.var()
