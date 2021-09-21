e9e7ebe5fc5b4a7302e040e1e3c84039ad3994a8
Patrick <hotatchang@gmail.com>
2021-09-22 00:07:14 +0800

    add History and Portfolio detail dataframe

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import portfolio as pf
 
   
 
-portf1 = pf.Portfolio()
+portf1 = pf.Portfolio("Pat",100000000)
 portf1.buyStock('2020-01-01','AAPL',130, 1000)
 portf1.buyStock('2020-02-01','GOOG',200, 500)
 portf1.buyStock('2020-03-01','NFLX',120, 500)
 portf1.buyStock('2020-03-01','NFLX',110, 500)
 portf1.buyStock('2020-03-01','NFLX',160, 500)
-portf1.sellStock('AAPL',150,200)
-portf1.sellStock('AAPL',150,800)
+portf1.sellStock('2020-03-01','AAPL',150,200)
+portf1.sellStock('2020-03-01','AAPL',150,800)
 print(portf1.getPortfolio())
 
 
--- portfolio.py
+++ portfolio.py
@@ -6,15 +6,19 @@ import numpy as np
 import math
 
 class Portfolio:
-    noOfStock = int(0);
-    invested = float(0);
-    turnover = float(0);
-    PandL = float(0);
+    portfolioName = "";
+    noOfStock = int(0)
+    account_value = float(0)
+    invested = float(0)
+    turnover = float(0)
+    PandL = float(0)
     portf = pd.DataFrame(columns=['Date','Stock','Price','No. of transaction','Value','Average Return(1yr)','Volatility(1yr)','Current Value','Spot Price',"%change"])
+    history = pd.DataFrame(columns=['Date','Stock','Action','Price','No. of Transaction'])
+    incomeStatrment = pd.DataFrame(columns=['Portfolio Name','Portfolio Expected Return','Portfolio Return','Portfolio Risk'])
 
-    def __init__(self):
-        self.invested = 0;
-        self.noOfStock = 0;
+    def __init__(self,portfolio_name,account_value):
+        self.account_value = account_value
+        self.portfolioName = portfolio_name
 
     def buyStock(self, date, stock, price, noOfTransaction):
         filt = self.portf['Stock'] == stock
@@ -38,8 +42,10 @@ class Portfolio:
             AverageReturn = str(round((self.AverageReturn(stock,df)-1)*100,2)) + '%'
             self.portf.loc[self.noOfStock-1] = [date,stock,float(price),int(noOfTransaction),float(price*noOfTransaction),AverageReturn,risk,0,0,0]
         self.update(self.portf)
+        self.account_value -= price*noOfTransaction
+        self.addHistory("Buy",date,stock,price,noOfTransaction)
 
-    def sellStock(self,stock,price,noOftransaction):
+    def sellStock(self,date,stock,price,noOftransaction):
         filt = self.portf['Stock'] == stock
         if(self.portf.loc[filt,'Stock'].any()):
             self.turnover = price*noOftransaction
@@ -48,10 +54,9 @@ class Portfolio:
             self.portf.loc[filt,'Value'] -= price*noOftransaction
              
             if(int(self.portf.loc[filt,'No. of transaction']) == 0):
-                print(self.portf[self.portf['Stock']==stock].index[0])
-
                 self.portf.drop([self.portf[self.portf['Stock']==stock].index[0]])
-    
+            self.account_value += price*noOftransaction
+            self.addHistory("Sell",date,stock,price,noOftransaction)
             
 
     def update(self,df):
@@ -62,10 +67,12 @@ class Portfolio:
             df = web.DataReader(stocks, data_source='yahoo',start= weekAgo,end= today)['Adj Close']
             filt = self.portf['Stock'] == stocks
             self.portf.loc[filt,'Spot Price'] = round(df.iloc[len(df)-1],2)  
-            self.portf.loc[filt,'%change'] = str(round(((df.iloc[len(df)-1] / float(self.portf.loc[filt,'Price']))-1)*100,2))+'%'
-            
+            self.portf.loc[filt,'%change'] = str(round(((df.iloc[len(df)-1] / float(self.portf.loc[filt,'Price']))-1)*100,2))+'%'          
             self.portf.loc[filt,'Current Value'] = round(df.iloc[len(df)-1],2) *self.portf.loc[filt,'No. of transaction']
-        
+    
+    def addHistory(self, action, date, stock, price, noOfTranssaction):
+        self.history.loc[len(self.history)] = [date,stock,action,price,noOfTranssaction]
+        print(self.history)
 
     def getPortfolio(self):
         return self.portf
@@ -82,4 +89,5 @@ class Portfolio:
         var = df.var()
         risk = math.sqrt(var)
         return risk
-
+    #def getPortfolioDetail(self):
+        
